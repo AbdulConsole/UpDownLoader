c245c174b12c825a73f32bc3866eaf797af5a653
AbdulConsole <hzwedt@gmail.com>
2019-11-16 22:42:18 +0100

    Added an Upload Page

---
+++ upload.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Upload</title>
+</head>
+
+<body>
+<form enctype="multipart/form-data" action="run.php" method="post">
+  <div align="center">
+    <p> 
+      Upload</p>
+    <p><input name="Filedata" type="file" /><br />
+      <input type="submit" value="Upload" />
+    </p>
+  </div>
+</form>
+
+</body>
+</html>
