d18b3cb22b73c941e73b68ad7c7c8a28f334ff01
AbdulConsole <hzwedt@gmail.com>
2019-11-16 22:39:51 +0100

    Added an index Page

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Upload | Download Portal</title>
+</head>
+<body style="text-align: center;">
+	<h1>Upload | Download Portal</h1>
+	<a href="upload.php"><input type="submit" value="Upload Files"></a>
+	<a href="./uploads"><input type="submit" value="View/Download Files"></a>
+</body>
+</html>
