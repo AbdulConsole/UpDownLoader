9b98f0cbab05f7c2e15d0603a52ca26c7042e09f
AbdulConsole <hzwedt@gmail.com>
2019-11-16 22:45:47 +0100

    Created PHP script for Upload run.php

---
+++ run.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Run | PHP</title>
+</head>
+
+<body style="text-align: center;">
+
+<?php
+
+	$uploaddir = 'C:\xampp\htdocs\telco\uploads\\';
+	$uploadfile = $uploaddir . $_FILES['Filedata']['name'];
+
+
+	print "<pre>";
+	if (move_uploaded_file($_FILES['Filedata']['tmp_name'], $uploadfile)) {
+	    echo "The file ". basename( $_FILES['Filedata']['name']). " has been uploaded";
+	} else{
+	    echo "There was an error uploading the file, please try again!";
+	}
+
+?> 
+<a href="upload.php">Upload</a> <a href="./uploads">View</a>
+</body>
+</html>
